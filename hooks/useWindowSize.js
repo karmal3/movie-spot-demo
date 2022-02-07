d3e55d7dba017c7a0277d5b5d90ad18d8b8cd5b6
karmal3 <karolis.maliauskas@gmail.com>
2022-02-07 15:25:46 +0200

    Modified hook for checking screen height/width

--- hooks/useWindowSize.js
+++ hooks/useWindowSize.js
@@ -1,12 +1,18 @@
 import { useEffect, useState } from 'react';
 
 export default function useWindowMobile() {
-
-
     // Initialize state with undefined width/height so server and client renders match
     const [isMobile, setIsMobile] = useState(false);
+    const [windowSize, setWindowSize] = useState({
+        width: undefined,
+        height: undefined,
+    });
     useEffect(() => {
         function handleResize() {
+            setWindowSize({
+                width: window.innerWidth,
+                height: window.innerHeight,
+            });
             if (window.innerWidth < 640) {
                 setIsMobile(true);
             } else {
@@ -18,6 +24,5 @@ export default function useWindowMobile() {
         handleResize();
         return () => window.removeEventListener("resize", handleResize);
     }, []); // Empty array ensures that effect is only run on mount
-    return isMobile;
-
+    return { isMobile, windowSize };
 }
